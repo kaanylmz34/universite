345493ff7d2ad8cd5b2fc24065028d69cb03eb92
Taha Kaan Yılmaz <kaanylmz34@icloud.com>
2023-09-03 02:32:39 +0300

    Admission Modülü çalışmaları

--- Modules/Admission/Resources/views/layouts/master.blade.php
+++ Modules/Admission/Resources/views/layouts/master.blade.php
@@ -4,16 +4,26 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
         <title>Module Admission</title>
-
-       {{-- Laravel Vite - CSS File --}}
-       {{-- {{ module_vite('build-admission', 'Resources/assets/sass/app.scss') }} --}}
-
     </head>
     <body>
+        <header class="bg-[#11367f] text-white">
+            <div class="container mx-auto flex gap-4">
+                <a href="/" class="logo text-4xl py-4 px-8 bg-red-700 text-white inline-block">
+                    Universite
+                </a>
+                <div class="py-4 flex items-center">
+                    Admission Office
+                </div>
+                <div class="flex gap-12 ml-auto [&>*]:py-4 items-center">
+                    <a href="">Policies & Conditions</a>
+                    <a href="">Campus Life</a>
+                    <a href="">FAQ</a>
+                    <a href="" class="bg-slate-300 text-slate-600 px-3 rounded-lg">Apply Request</a>
+                </div>
+            </div>
+        </header>
         @yield('content')
-
-        {{-- Laravel Vite - JS File --}}
-        {{-- {{ module_vite('build-admission', 'Resources/assets/js/app.js') }} --}}
     </body>
 </html>
