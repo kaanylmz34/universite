77a6ed82c0c80a07109597833e2f0e0a4e4510f8
Taha Kaan Yılmaz <kaanylmz34@icloud.com>
2023-09-03 17:00:24 +0300

    Tema tasarımı

--- Modules/Admission/Resources/views/index.blade.php
+++ Modules/Admission/Resources/views/index.blade.php
@@ -1,7 +1,9 @@
 @extends('admission::layouts.master')
 
 @section('content')
-    <div class="w-full h-[calc(100vh-72px)] bg-[url(/public/stock/admission.jpg)] flex items-end">
+    
+<section id="head">
+<div class="w-full h-[calc(100vh-120px)] bg-[url(/public/stock/admission.jpg)] flex items-end">
         <div class="w-full h-[60%] bg-gradient-to-t from-black to-transparent text-white">
             <div class="container mx-auto flex h-full">
                 <div class="w-[100%] flex flex-col justify-center text-center items-center">
@@ -10,7 +12,7 @@
                     <div class="text-4xl">Come here to start your career.</div>
                 </div>
                 <div class="w-[100%] flex justify-end items-end text-black">
-                    <div class="w-[75%] bg-white">
+                    <div class="w-[75%] bg-white shadow-lg">
                         <div class="border-b p-6 text-center font-bold">REQUEST MORE INFO</div>
                         <div class="p-6">
                             <div class="flex flex-col gap-6 w-full">
@@ -36,4 +38,24 @@
             </div>
         </div>
     </div>
+</section>
+
+<section id="introduction-sport-activites" class="h-[calc(100vh-120px)] bg-blue-100 text-blue-700 flex flex-col gap-4 items-center">
+    <div class="bg-blue-700 text-white w-full py-12 text-center">
+        <div class="logo-font text-8xl">NSA SPORT MEMBERSHIP</div>
+        <div class="logo-font text-3xl">Our University has Official NSA Membership</div>
+    </div>
+    <div class="h-full flex items-center">
+        <div class="flex gap-8 mx-auto w-[75%] mt-12">
+            <div class="w-[30%]">
+                <img src="/stock/championship.png" alt="Our Basketball NSA Championship" class="rounded-full w-[256px] h-[256px]"/>
+            </div>
+            <div class="w-[100%] text-red-700">
+                <div class="text-6xl font-bold logo-font drop-shadow-[6px_6px_0_#FAAB2A]">The Universite 2017 NSA Basketball League Final Event Championship</div>
+                <div class="text-3xl italic mt-6">We won the final challenge that is incredible at 2017 NSA Basketball League.</div>
+            </div>
+        </div>
+    </div>
+</section>
+
 @endsection
--- Modules/Admission/Resources/views/layouts/master.blade.php
+++ Modules/Admission/Resources/views/layouts/master.blade.php
@@ -8,7 +8,7 @@
         <title>Module Admission</title>
     </head>
     <body>
-        <header class="bg-[#11367f] text-white">
+        <header class="bg-blue-900 text-white fixed w-full">
             <div class="container mx-auto flex gap-4">
                 <a href="/" class="logo text-4xl py-4 px-8 bg-red-700 text-white inline-block">
                     Universite
@@ -24,6 +24,17 @@
                 </div>
             </div>
         </header>
+        <div class="bg-white shadow-lg fixed w-full top-[72px]">
+            <div class="container [&>*]:py-4 flex gap-8 mx-auto text-xs text-slate-600">
+                <a href="#">AWARDS</a>
+                <a href="#">ACCOMPLISHMENTS</a>
+                <a href="#">INTRODUCE YOURSELF</a>
+                <a href="#">ABOUT ÜNİVERSİTE</a>
+                <a href="#">CONTACT US</a>
+                <a href="#">STUDENT OFFICE</a>
+            </div>
+        </div>
+        <div class="h-[120px]"></div>
         @yield('content')
     </body>
 </html>
