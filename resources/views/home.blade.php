2d3152c959943abaf1ba5a05b3f692b5f8d8cc62
Taha Kaan Yılmaz <kaanylmz34@icloud.com>
2023-09-08 01:41:38 +0300

    üniversite ana sayfa güncelleme

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+        <title>Üniversite - One of the Top Universities around the world</title>
+    </head>
+    <body>
+        <header class="flex">
+            <div class="header-foreground-left">
+                <div class="w-full overflow-hidden">
+                    <div class="main-header-left"></div>
+                </div>
+            </div>
+            <div class="header-foreground-right">
+                <div class="w-full overflow-hidden">
+                    <div class="main-header-right"></div>
+                </div>
+            </div>
+        </header>
+    </body>
+</html>
